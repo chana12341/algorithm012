d4cc915c5680b0daca6bb2830b9f1d9bdc7bf3bc
chana123 <1471692574@qq.com>
2020-07-19 19:27:44 +0800

    leetcode 242:有效的字母异位词

---
+++ Week_02/IsAnaGram.java
@@ -0,0 +1,18 @@
+//有效字母异位词
+class IsAnaGram{
+	public boolean isAnagram(String s, String t) {
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+        if(ss.length != tt.length) {
+            return false;
+        }
+        for(int i = 0; i<ss.length ;i++) {
+            if(ss[i] != tt[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
