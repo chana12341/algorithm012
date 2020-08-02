12b2b00020bf23a70848a2af71e693e9d38be74a
chana123 <1471692574@qq.com>
2020-08-02 21:37:29 +0800

    leetcode 455:分发饼干

---
+++ Week_04/LeetCode_455.java
@@ -0,0 +1,17 @@
+class LeetCode_455{
+	//分发饼干
+	public int findContentChildren(int[] g, int[] s) {
+        if (g == null || s == null)
+         return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gi = 0, si = 0;
+        while (gi <  g.length && si <  s.length) {
+            if ( g[gi] <= s[si]) {
+                gi++;
+            }
+            si++;
+        }
+        return gi;
+    }
+}
